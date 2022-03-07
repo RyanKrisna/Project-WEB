b412c4cb3c34eafdc2cc309a650f2254396c7305
RyanKrisna <ryankrisna13@gmail.com>
2022-03-07 21:01:11 +0800

    aritmatika

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AritmatikaController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,3 +16,6 @@ use App\Http\Controllers\HomeController;
 */
 
 Route::get('/',[HomeController::class, 'index']);
+Route::get('tugas1',[AritmatikaController::class, 'tugas1'])->name('tugas1');
+Route::get('tugas2',[AritmatikaController::class, 'tugas2'])->name('tugas2');
+
